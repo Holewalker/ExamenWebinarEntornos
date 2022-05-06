dfefad89e780c09a1ad38cffa18c0ab0e02d222f
Daniel G <dani75esp@gmail.com>
2022-05-06 18:33:11 +0200

    -clase Baraja

---
+++ src/main/java/Baraja.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class Baraja {
+
+    ArrayList<Carta> listaCartas;
+    static int NUM_CARTAS = 52;
+
+    private void PaqueteCartas() {
+    }
+
+    private void Barajar() {
+
+    }
+
+    private Carta cogerCarta(){
+        Carta carta = new Carta("a","a");
+        return carta;
+    }
+
+    private ArrayList<Carta>  repartirMano(){
+        ArrayList<Carta> mano = null;
+        return mano;
+    }
+
+}
